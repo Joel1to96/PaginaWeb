dd1ef9a44a177e3819a363d6f271c0ff746aada8
Joel1to96 <100208680+Joel1to96@users.noreply.github.com>
2024-11-24 00:51:20 +0000

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -10,16 +10,46 @@
 
   
   // Modo Oscuro (opcional)
-  const darkModeToggle = document.getElementById('dark-mode-toggle');
-  darkModeToggle.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-      const icon = darkModeToggle.querySelector('i');
-      if (document.body.classList.contains('dark-mode')) {
-          icon.classList.remove('fa-moon');
-          icon.classList.add('fa-sun');
-      } else {
-          icon.classList.remove('fa-sun');
-          icon.classList.add('fa-moon');
-      }
-  });
-  
+  // Modo Oscuro
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    // Verificar si hay una preferencia almacenada en localStorage
+    const darkModePreference = localStorage.getItem('darkMode');
+
+    if (darkModePreference === 'enabled') {
+        enableDarkMode();
+    }
+
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            const isDarkModeEnabled = document.body.classList.contains('dark-mode');
+            if (isDarkModeEnabled) {
+                disableDarkMode();
+            } else {
+                enableDarkMode();
+            }
+        });
+    }
+
+    function enableDarkMode() {
+        document.body.classList.add('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+        if (icon) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        }
+        localStorage.setItem('darkMode', 'enabled');
+    }
+
+    function disableDarkMode() {
+        document.body.classList.remove('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+        if (icon) {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
